970381cdad433df279defe1d17126bf41722a0b9
MarcosLara38 <78185212+MarcosLara38@users.noreply.github.com>
2021-03-08 18:33:13 -0800

    First Draft

---
+++ 2D Game/Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    public GameObject PauseMenuUI;
+    public static bool GameIsPaused = false;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (GameIsPaused)
+            {
+                ResumeGame();
+            }
+            else
+            {
+                Debug.Log("esc is pressed");
+                PauseGame();
+            }
+        }
+    }
+
+    public void PauseGame()
+    {
+        PauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        GameIsPaused = true;
+    }
+
+    public void ResumeGame()
+    {       
+        PauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        GameIsPaused = false;
+    }
+
+    public void MainMenu()
+    {
+        PauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        SceneManager.LoadScene(0);
+    }
+
+   
+}
---
+++ 2D Game/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float MovementSpeed = 1f;                
+    float movement;     
+    bool jump = false;
+    bool groundCheck = true;
+    bool attacking = false;
+    public float JumpForce = 1f;
+    public Rigidbody2D rigidbody;
+    public Animator animator;
+
+    // Update is called once per frame
+    void Update()
+    {
+        movement = Input.GetAxisRaw("Horizontal");
+
+        animator.SetFloat("Speed", Mathf.Abs(movement));
+        if (Input.GetButtonDown("Jump") && Mathf.Abs(rigidbody.velocity.y) < 0.001f)
+        {
+            jump = true;
+            groundCheck = false;
+        }
+        if (Input.GetMouseButtonDown(0))
+        {
+            attacking = true;
+            Debug.Log("Attack");
+        }
+    }
+
+    void FixedUpdate()
+    {
+        transform.position += new Vector3(movement, 0, 0) * Time.deltaTime * MovementSpeed;
+        if(jump == true)
+        {
+            rigidbody.AddForce(new Vector2(0, JumpForce), ForceMode2D.Impulse);
+            jump = false;
+            groundCheck = true;
+        }
+
+        if (attacking)
+        {
+            animator.SetBool("IsAttacking", true);
+            attacking = false;          
+        }
+        else if (attacking == false)
+        {
+            animator.SetBool("IsAttacking", false);
+        }
+    }
+
+   
+}
