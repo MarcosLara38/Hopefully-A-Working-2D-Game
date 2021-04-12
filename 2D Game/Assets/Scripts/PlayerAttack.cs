3fdfc7c9b608f494b5cd6e0a89accce50747e1a2
eosornio <78441698+eosornio@users.noreply.github.com>
2021-04-11 22:59:06 -0700

    Improved some code

--- 2D Game/Assets/Scripts/PlayerAttack.cs
+++ 2D Game/Assets/Scripts/PlayerAttack.cs
@@ -4,12 +4,49 @@ using UnityEngine;
 
 public class PlayerAttack : MonoBehaviour
 {
-    void OnTriggerEnter2D(Collider2D other)
+    //void OnTriggerEnter2D(Collider2D other)
+    //{
+        //Debug.Log("test there has been an collision");
+        //if ((other.gameObject.tag == "Enemy" || other.gameObject.name == "Enemy"))
+        //{
+            //other.gameObject.GetComponent<EnemyMove>()._lifePoints -= 1;
+        //}
+    //}
+    private float timeBtwAttack;
+    public float startTimeBtwAttack;
+    public LayerMask whatIsEnemies;
+
+    public Transform attackPos;
+    public float attackRange;
+    public int damage;
+
+    void Update()
     {
-        Debug.Log("test there has been an collision");
-        if ((other.gameObject.tag == "Enemy" || other.gameObject.name == "Enemy"))
+        if(timeBtwAttack <= 0)
         {
-            other.gameObject.GetComponent<EnemyMove>()._lifePoints -= 1;
+            //then you can attack
+            if(Input.GetMouseButtonDown(0))
+            {
+                //camAnim.SetTrigger("shake");
+                //playerAnim.SetTrigger("attack");
+                Collider2D[] enemiesToDamage = Physics2D.OverlapCircleAll(attackPos.position, attackRange, whatIsEnemies);
+                for(int i = 0; i < enemiesToDamage.Length; i++)
+                {
+                    enemiesToDamage[i].GetComponent<EnemyAI>().TakeDamage(damage);
+
+                }
+            }
+            timeBtwAttack = startTimeBtwAttack;
+        }
+        else
+        {
+            timeBtwAttack -= Time.deltaTime;
+
+        }
+        void OnDrawGizmosSelected()
+        {
+            Gizmos.color = Color.red;
+            Gizmos.DrawWireSphere(attackPos.position, attackRange);
         }
     }
 }
