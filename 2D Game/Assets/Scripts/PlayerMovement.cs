a4628c2bb7497eefbe69c3fdace0b76ef6c0c0e4
Carolina Martinez <78441764+cmartinez136@users.noreply.github.com>
2021-05-15 13:17:21 -0700

    Revert "Revert "New sprite and animations""
    This reverts commit d651c2f5f066a77f08f7e11926dccd24b873900e.

---
+++ 2D Game/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public Animator animator;
+    public float runSpeed = 1f;
+    public float jumpForce = 1f;
+
+    private Rigidbody2D rBody;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rBody = GetComponent<Rigidbody2D>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var move = Input.GetAxis("Horizontal");
+        transform.position = transform.position + new Vector3(move * runSpeed * Time.deltaTime, 0, 0);
+
+    }
+
+}
